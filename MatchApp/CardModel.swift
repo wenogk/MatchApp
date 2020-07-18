2854345c51f45819f8669de46ab5c128b9a8f477
wenogk <drf325@nyu.edu>
2020-07-18 14:28:51 +0400

    set up stuff

---
+++ MatchApp/CardModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardModel.swift
+//  MatchApp
+//
+//  Created by Romeno Wenogk Fernando on 18/07/2020.
+//  Copyright © 2020 Romeno Wenogk Fernando. All rights reserved.
+//
+
+import Foundation
+
+
+class CardModel {
+    func getCards() {
+        
+    }
+}
